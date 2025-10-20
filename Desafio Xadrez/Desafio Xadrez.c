a12a1ad073f518e34fd81e4b2cf744ab345e310d
Gustavo-Vernon <02gustavolima@gmail.com>
2025-10-20 22:50:04 +0000

    // Primeira versao sendo o nivel novato

---
+++ Desafio Xadrez/Desafio Xadrez.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <string.h>
+int main () {
+
+      int rainha = 8 ;
+      int casas = 0;
+      int bispo = 5;
+      int torre = 5;
+      int x = 0;
+      int y = 0;
+
+      printf("Movimento x significa horizontal e o movimento y significa vertical\n");
+      printf("Torre comeca na posicao %d\n", x);
+
+      while (casas < torre) {
+            x++;
+            casas++;
+            printf("A torre andou %d casas para a direita\n", x);
+      }
+
+      printf("\n");
+     
+      // Movimentacao da peca bispo;
+      // Primeiro determinando novamente os valores das casas e direcoes
+      casas = 0;
+      x = 0;
+      y = 0;
+
+      printf("A peca agora eh o bispo comecando na posicao %d em x e %d em y \n", x, y);
+      do
+      {
+            x++;
+            y++;
+            casas++;
+            printf("O bispo andou %d casas para cima e %d para a direita\n", y, x);
+      }
+       while (casas < bispo); 
+       printf("O bispo terminou na posicao %d y e %d x \n", y, x);
+
+       printf("\n");
+
+       //Movimentacao da rainha sendo feito o inverso
+       //Comeca da ultima posicao (8) e retorna para o 0
+      
+       x = 8;
+       printf("A rainha comeca na posicao %d\n", rainha);
+       
+
+    for (casas = 0; x > 0; casas++) {
+        x--; 
+        printf("A rainha andou para a esquerda, posição atual: %d\n", x);
+    }
+
+    printf("A rainha terminou na posicao %d\n", x);
+
+    return 0;
+      
+
+      
+      
+      
+
+      
+      
+      
+      
+      
+      
+      
+      return 0;
+}
